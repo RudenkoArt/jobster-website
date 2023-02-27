06e935178cdef15a4e614f5961c57c2bb633f25d
RudenkoArt <rudnkoart@gmail.com>
2023-02-27 14:59:48 +0200

    implement react router 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Landing from './pages/Landing';
+import { Landing, Error, Dashboard, Register } from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='landing' element={<Landing />} />
+        <Route path='register' element={<Register />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   );
 };
 
