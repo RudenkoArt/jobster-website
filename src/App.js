9cc1ef750c88181d094f8da3e9aa7ee3d79b846d
RudenkoArt <rudnkoart@gmail.com>
2023-03-06 14:29:53 +0200

    wrapped SharedLayout with protected route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Landing, Error, Register } from './pages';
+import { Landing, Error, Register, ProtectedRoute } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,7 +14,14 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<SharedLayout />}>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path='all-jobs' element={<AllJobs />} />
           <Route path='add-job' element={<AddJob />} />
